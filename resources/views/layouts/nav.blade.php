272b2c61bdf51ec8f5cf6ed61f9650b0e160ba61
Bogdan <garfield1333@gmail.com>
2017-04-21 12:32:16 +0300

    Email option added

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -4,6 +4,25 @@
             <a class="nav-link active" href="/">Home</a>
             <a class="nav-link" href="/">New products</a>
             <a class="nav-link" href="/">About</a>
+
+            @if (Auth::check())
+
+                <a class="nav-link ml-auto" href="/adminpage">{{Auth::user()->name}}</a>
+                <a href="{{ route('logout') }}"
+                   onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                    Logout
+                </a>
+
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    {{ csrf_field() }}
+                </form>
+
+            @elseif(!Auth::check())
+                <a class="nav-link ml-auto" href="/login">Login</a>
+            @endif
+
         </nav>
     </div>
 </div>
+
